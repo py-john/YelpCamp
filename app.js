cea903b11d1a1dbcd1a1080b8f7afef598ff9ef8
John <john@debian.com>
2018-08-22 19:00:30 -0700

    add authentication functionality

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-var bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    express    = require("express"),
-    app        = express();
-    Campground = require("./models/campground");
-    Comment    = require("./models/comment");
-    seedDB     = require("./seeds");
+var bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    express       = require("express"),
+    Campground    = require("./models/campground"),
+    Comment       = require("./models/comment"),
+    User          = require("./models/user"),
+    seedDB        = require("./seeds");
 
-
+// App config
+var app = express();
+var port = process.env.PORT || 3000
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-var port = process.env.PORT || 8080
+// Mongo
 var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
 mongoose.connect(url, { useNewUrlParser: true });
 // seedDB();
 
 
+// Passport configuration
+app.use(require("express-session")({
+    secret: process.env.SESSION_SECRET || "session secret string",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+// Routes
 app.get("/", function(req, res){
     res.render("landing");
 });
@@ -27,7 +49,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         } else {
-            res.render("campgrounds/index", {campgrounds: campgrounds});
+            res.render("campgrounds/index", {campgrounds: campgrounds, currentUser: req.user});
         }
     });
 });
@@ -68,7 +90,7 @@ app.get("/campgrounds/:id", function(req, res){
 //----------------
 // Comments routes
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err);
@@ -78,7 +100,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err);
@@ -96,6 +118,50 @@ app.post("/campgrounds/:id/comments", function(req, res){
         }
     });
 });
+
+// Auth Routes
+app.get("/register", function(req, res){
+    res.render("register");
+});
+// Sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+// Show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// Handle login logic
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "login"
+    }), function(req, res){
+});
+
+// Logout
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(port, function(){
     console.log("App is running at http://localhost:" + port);
